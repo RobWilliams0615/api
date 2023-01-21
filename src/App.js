c7b8c97bbd01f53f79e37e9d7697a928094e5b10
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2023-01-21 10:07:31 -0700

    my handleSubmit fucntion was in the wrong place, moved it within the main App function on App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import SearchBar from './components/SearchBar';
 import imageSearch from './api';
 import ImageList from './components/ImageList';
-
-const handleSubmit = async (term) => {
-  const result = await imageSearch(term);
-
-  console.log(result);
-};
+import { useState } from 'react';
 
 function App() {
+  const handleSubmit = async (term) => {
+    const result = await imageSearch(term);
+
+    console.log(result);
+  };
+
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
